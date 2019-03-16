232c5f27a365067a21f538fbcbabc45a23be5378
Aman Adhikari <amanadhikari2@gmail.com>
2019-03-16 19:23:04 +0530

    Added User Model

---
+++ api/src/main/java/com/collaborate/app/collaborateApp/model/UserModel.java
@@ -0,0 +1,88 @@
+package com.collaborate.app.collaborateApp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user_list")
+public class UserModel extends AuditModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3)
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(unique=true)
+    private String email;
+
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(unique=true)
+    private String username;
+
+    @NotBlank
+    @Column(unique=true)
+    private  String number;
+
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
